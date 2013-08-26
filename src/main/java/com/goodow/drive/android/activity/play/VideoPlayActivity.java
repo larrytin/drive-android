2560e649289d9055101cf86d3b1cf46dd818eee6
danhantao <danhantao@goodow.com>
2013-08-26 15:35:25 +0800

    修改乱码

--- src/main/java/com/goodow/drive/android/activity/play/VideoPlayActivity.java
+++ src/main/java/com/goodow/drive/android/activity/play/VideoPlayActivity.java
@@ -18,59 +18,58 @@ import android.widget.Toast;
 import com.goodow.android.drive.R;
 
 public class VideoPlayActivity extends Activity {
-  private final String TAG = this.getClass().getSimpleName();
-
   public static enum IntentExtraTagEnum {
     // mp4 资源名称
     MP4_NAME,
     // MP4资源完整path
     MP4_PATH
-  };
-
-  private MediaPlayer mediaPlayer = new MediaPlayer();
-  private String audioFilePath;
-  private SurfaceView surfaceView;
-  private int position;
-
-  @SuppressWarnings("deprecation")
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_video_player);
-
-    // 获取从外部传进来的 mp4资源完整路径
-    audioFilePath = getIntent().getStringExtra(
-        IntentExtraTagEnum.MP4_PATH.name());
-
-    // 获取从外部传进来的 mp4资源名字
-    String mp3Name = getIntent().getStringExtra(
-        IntentExtraTagEnum.MP4_NAME.name());
-
-    final TextView audioFileNameTextView = (TextView) this
-        .findViewById(R.id.video_file_name_textView);
-    audioFileNameTextView.setText(mp3Name);
-    surfaceView = (SurfaceView) this.findViewById(R.id.surfaceView);
-    surfaceView.getHolder().setFixedSize(176, 144);//设置Surface分辨率
-    surfaceView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    surfaceView.getHolder().addCallback(new SurfaceCallback());
-
-    mediaPlayer = new MediaPlayer();
-    ButtonClickListener listener = new ButtonClickListener();
-    ImageButton playButton = (ImageButton) this
-        .findViewById(R.id.play_ImageButton);
-    ImageButton pauseButton = (ImageButton) this
-        .findViewById(R.id.pause_ImageButton);
-    ImageButton resetButton = (ImageButton) this
-        .findViewById(R.id.reset_ImageButton);
-    ImageButton stopButton = (ImageButton) this
-        .findViewById(R.id.stop_ImageButton);
-    playButton.setOnClickListener(listener);
-    pauseButton.setOnClickListener(listener);
-    resetButton.setOnClickListener(listener);
-    stopButton.setOnClickListener(listener);
   }
 
+  private final class ButtonClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+      if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+        Toast.makeText(VideoPlayActivity.this, "SDCard不存在", Toast.LENGTH_SHORT).show();
+        return;
+      }
+
+      try {
+        switch (v.getId()) {
+          case R.id.play_ImageButton:
+            play();
+            break;
+
+          case R.id.pause_ImageButton:
+            if (mediaPlayer.isPlaying()) {
+              mediaPlayer.pause();
+            } else {
+              mediaPlayer.start();
+            }
+            break;
+          case R.id.reset_ImageButton:
+            if (mediaPlayer.isPlaying()) {
+              mediaPlayer.seekTo(0);
+            } else {
+              play();
+            }
+            break;
+          case R.id.stop_ImageButton:
+            if (mediaPlayer.isPlaying()) {
+              mediaPlayer.stop();
+            }
+            break;
+        }
+      } catch (Exception e) {
+        Log.e(TAG, e.toString());
+      }
+    }
+  };
+
   private final class SurfaceCallback implements SurfaceHolder.Callback {
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
       if (position > 0 && audioFilePath != null) {
@@ -85,11 +84,6 @@ public class VideoPlayActivity extends Activity {
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-        int height) {
-    }
-
-    @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
       if (mediaPlayer.isPlaying()) {
         position = mediaPlayer.getCurrentPosition();
@@ -97,46 +91,43 @@ public class VideoPlayActivity extends Activity {
       }
     }
   }
+  private final String TAG = this.getClass().getSimpleName();
+  private MediaPlayer mediaPlayer = new MediaPlayer();
+  private String audioFilePath;
 
-  private final class ButtonClickListener implements View.OnClickListener {
-    @Override
-    public void onClick(View v) {
-      if (!Environment.getExternalStorageState().equals(
-          Environment.MEDIA_MOUNTED)) {
-        Toast.makeText(VideoPlayActivity.this, "SDCard不存在", Toast.LENGTH_SHORT)
-            .show();
-        return;
-      }
+  private SurfaceView surfaceView;
 
-      try {
-        switch (v.getId()) {
-        case R.id.play_ImageButton:
-          play();
-          break;
+  private int position;
 
-        case R.id.pause_ImageButton:
-          if (mediaPlayer.isPlaying()) {
-            mediaPlayer.pause();
-          } else {
-            mediaPlayer.start();
-          }
-          break;
-        case R.id.reset_ImageButton:
-          if (mediaPlayer.isPlaying()) {
-            mediaPlayer.seekTo(0);
-          } else {
-            play();
-          }
-          break;
-        case R.id.stop_ImageButton:
-          if (mediaPlayer.isPlaying())
-            mediaPlayer.stop();
-          break;
-        }
-      } catch (Exception e) {
-        Log.e(TAG, e.toString());
-      }
-    }
+  @SuppressWarnings("deprecation")
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_video_player);
+
+    // 获取从外部传进来的 mp4资源完整路径
+    audioFilePath = getIntent().getStringExtra(IntentExtraTagEnum.MP4_PATH.name());
+
+    // 获取从外部传进来的 mp4资源名字
+    String mp3Name = getIntent().getStringExtra(IntentExtraTagEnum.MP4_NAME.name());
+
+    final TextView audioFileNameTextView = (TextView) this.findViewById(R.id.video_file_name_textView);
+    audioFileNameTextView.setText(mp3Name);
+    surfaceView = (SurfaceView) this.findViewById(R.id.surfaceView);
+    surfaceView.getHolder().setFixedSize(176, 144);// 设置Surface分辨率
+    surfaceView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    surfaceView.getHolder().addCallback(new SurfaceCallback());
+
+    mediaPlayer = new MediaPlayer();
+    ButtonClickListener listener = new ButtonClickListener();
+    ImageButton playButton = (ImageButton) this.findViewById(R.id.play_ImageButton);
+    ImageButton pauseButton = (ImageButton) this.findViewById(R.id.pause_ImageButton);
+    ImageButton resetButton = (ImageButton) this.findViewById(R.id.reset_ImageButton);
+    ImageButton stopButton = (ImageButton) this.findViewById(R.id.stop_ImageButton);
+    playButton.setOnClickListener(listener);
+    pauseButton.setOnClickListener(listener);
+    resetButton.setOnClickListener(listener);
+    stopButton.setOnClickListener(listener);
   }
 
   // 播放
