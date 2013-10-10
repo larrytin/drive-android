be3a037329cfc4ee18a3108d3c940b6eaa8df672
danhantao <danhantao@goodow.com>
2013-10-10 09:12:53 +0800

    change

--- src/main/java/com/goodow/drive/android/activity/MainActivity.java
+++ src/main/java/com/goodow/drive/android/activity/MainActivity.java
@@ -30,13 +30,9 @@ import com.goodow.realtime.android.ServerAddress;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -208,17 +204,17 @@ public class MainActivity extends RoboActivity {
   @Override
   public boolean onKeyDown(int keyCode, KeyEvent event) {
     switch (keyCode) {
-    case KeyEvent.KEYCODE_BACK:
-      if (null != currentFragment) {
-        currentFragment.backFragment();
+      case KeyEvent.KEYCODE_BACK:
+        if (null != currentFragment) {
+          currentFragment.backFragment();
+
+          return true;
+        }
+      case KeyEvent.KEYCODE_HOME:
 
         return true;
-      }
-    case KeyEvent.KEYCODE_HOME:
-
-      return true;
-    default:
-      break;
+      default:
+        break;
     }
 
     return super.onKeyDown(keyCode, event);
@@ -239,23 +235,25 @@ public class MainActivity extends RoboActivity {
       }
     } else if (item.getItemId() == 0) {
       // 退出程序 取消
-      new AlertDialog.Builder(this).setPositiveButton(R.string.unsaved_dialog_cancel, new DialogInterface.OnClickListener() {
+      new AlertDialog.Builder(this).setPositiveButton(R.string.unsaved_dialog_cancel,
+          new DialogInterface.OnClickListener() {
 
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
-        }
-        // 确定
-      }).setNegativeButton(R.string.trix_sheets_tab_menu_ok, new DialogInterface.OnClickListener() {
+            }
+            // 确定
+          }).setNegativeButton(R.string.trix_sheets_tab_menu_ok,
+          new DialogInterface.OnClickListener() {
 
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-          GlobalDataCacheForMemorySingleton.getInstance.setUserId(null);
-          GlobalDataCacheForMemorySingleton.getInstance.setAccess_token(null);
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+              GlobalDataCacheForMemorySingleton.getInstance.setUserId(null);
+              GlobalDataCacheForMemorySingleton.getInstance.setAccess_token(null);
 
-          ToolsFunctionForThisProgect.quitApp(MainActivity.this);
-        }
-      }).setTitle("温馨提示").setMessage(R.string.back_dailogMessage).create().show();
+              ToolsFunctionForThisProgect.quitApp(MainActivity.this);
+            }
+          }).setTitle("温馨提示").setMessage(R.string.back_dailogMessage).create().show();
     }
 
     return true;
@@ -316,8 +314,8 @@ public class MainActivity extends RoboActivity {
           }
         });
 
-        linearLayout.addView(newTextView, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(newTextView, new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
       }
     }
 
@@ -337,7 +335,8 @@ public class MainActivity extends RoboActivity {
       visToInvis.setDuration(500);
       visToInvis.setInterpolator(accelerator);
 
-      final ObjectAnimator invisToVis = ObjectAnimator.ofFloat(dataDetailLayout, "rotationY", -90f, 0f);
+      final ObjectAnimator invisToVis =
+          ObjectAnimator.ofFloat(dataDetailLayout, "rotationY", -90f, 0f);
       invisToVis.setDuration(500);
       invisToVis.setInterpolator(decelerator);
       visToInvis.addListener(new AnimatorListenerAdapter() {
@@ -371,7 +370,8 @@ public class MainActivity extends RoboActivity {
   }
 
   public void showChangeUserDialog() {
-    final View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_change_user_layout, null);
+    final View dialogView =
+        LayoutInflater.from(this).inflate(R.layout.dialog_change_user_layout, null);
     final Dialog dialog = new Dialog(this, R.style.AlertDialog);
     dialog.show();
     Window window = dialog.getWindow();
@@ -406,9 +406,10 @@ public class MainActivity extends RoboActivity {
           }
 
           // 一切OK
-          String[] params = { username, password };
+          String[] params = {username, password};
           Account account = provideDevice(GlobalConstant.REALTIME_SERVER);
-          final LoginNetRequestTask loginNetRequestTask = new LoginNetRequestTask(MainActivity.this, dialog, account);
+          final LoginNetRequestTask loginNetRequestTask =
+              new LoginNetRequestTask(MainActivity.this, dialog, account);
           SimpleProgressDialog.show(MainActivity.this, new OnCancelListener() {
             @Override
             public void onCancel(DialogInterface dialog) {
@@ -447,22 +448,22 @@ public class MainActivity extends RoboActivity {
       }
 
       switch (doc) {
-      case LESSONDOCID:
-        newFragment = lessonListFragment;
+        case LESSONDOCID:
+          newFragment = lessonListFragment;
 
-        break;
-      case FAVORITESDOCID:
-        newFragment = dataListFragment;
+          break;
+        case FAVORITESDOCID:
+          newFragment = dataListFragment;
 
-        break;
-      case OFFLINEDOCID:
-        newFragment = offlineListFragment;
+          break;
+        case OFFLINEDOCID:
+          newFragment = offlineListFragment;
 
-        break;
-      default:
-        newFragment = dataListFragment;
+          break;
+        default:
+          newFragment = dataListFragment;
 
-        break;
+          break;
       }
 
       if (null == newFragment) {
@@ -488,30 +489,31 @@ public class MainActivity extends RoboActivity {
     GlobalDataCacheForMemorySingleton.getInstance.addActivity(this);
 
     // 添加捕获全局异常的处理方案
-    // Thread.currentThread().setUncaughtExceptionHandler(OverallUncaughtException.OVERALLUNCAUGHTEXCEPTION);
-    //
-    // OverallUncaughtException.OVERALLUNCAUGHTEXCEPTION.setLoginAgain(new LoginAgain() {
-    // @Override
-    // public void login(String errorinfo) {
-    // SharedPreferences sharedPreferences =
-    // getSharedPreferences(LoginNetRequestTask.LOGINPREFERENCESNAME, Activity.MODE_PRIVATE);
-    // String userName = sharedPreferences.getString(LoginNetRequestTask.USERNAME, "");
-    // String passWord = sharedPreferences.getString(LoginNetRequestTask.PASSWORD, "");
-    //
-    // Log.e(TAG, errorinfo);
-    //
-    // Intent intent = new Intent(MainActivity.this, MainActivity.class);
-    // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    // intent.putExtra(LoginNetRequestTask.USERNAME, userName);
-    // intent.putExtra(LoginNetRequestTask.PASSWORD, passWord);
-    //
-    // GlobalDataCacheForMemorySingleton.getInstance.exit();
-    //
-    // MainActivity.this.startActivity(intent);
-    //
-    // android.os.Process.killProcess(android.os.Process.myPid());
-    // }
-    // });
+    Thread.currentThread().setUncaughtExceptionHandler(
+        OverallUncaughtException.OVERALLUNCAUGHTEXCEPTION);
+
+    OverallUncaughtException.OVERALLUNCAUGHTEXCEPTION.setLoginAgain(new LoginAgain() {
+      @Override
+      public void login(String errorinfo) {
+        SharedPreferences sharedPreferences =
+            getSharedPreferences(LoginNetRequestTask.LOGINPREFERENCESNAME, Activity.MODE_PRIVATE);
+        String userName = sharedPreferences.getString(LoginNetRequestTask.USERNAME, "");
+        String passWord = sharedPreferences.getString(LoginNetRequestTask.PASSWORD, "");
+
+        Log.e(TAG, errorinfo);
+
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(LoginNetRequestTask.USERNAME, userName);
+        intent.putExtra(LoginNetRequestTask.PASSWORD, passWord);
+
+        GlobalDataCacheForMemorySingleton.getInstance.exit();
+
+        MainActivity.this.startActivity(intent);
+
+        android.os.Process.killProcess(android.os.Process.myPid());
+      }
+    });
 
     actionBar = getActionBar();
 
@@ -533,9 +535,10 @@ public class MainActivity extends RoboActivity {
       String userName = extras.getString(LoginNetRequestTask.USERNAME);
       String passWord = extras.getString(LoginNetRequestTask.PASSWORD);
 
-      String[] params = { userName, passWord };
+      String[] params = {userName, passWord};
       Account account = provideDevice(GlobalConstant.REALTIME_SERVER);
-      final LoginNetRequestTask loginNetRequestTask = new LoginNetRequestTask(MainActivity.this, null, account);
+      final LoginNetRequestTask loginNetRequestTask =
+          new LoginNetRequestTask(MainActivity.this, null, account);
       SimpleProgressDialog.show(MainActivity.this, new OnCancelListener() {
         @Override
         public void onCancel(DialogInterface dialog) {
@@ -653,12 +656,13 @@ public class MainActivity extends RoboActivity {
   @Singleton
   private Account provideDevice(@ServerAddress String serverAddress) {
     Account.Builder endpointBuilder =
-        new Account.Builder(AndroidHttp.newCompatibleTransport(), new JacksonFactory(), new HttpRequestInitializer() {
-          @Override
-          public void initialize(HttpRequest httpRequest) {
+        new Account.Builder(AndroidHttp.newCompatibleTransport(), new JacksonFactory(),
+            new HttpRequestInitializer() {
+              @Override
+              public void initialize(HttpRequest httpRequest) {
 
-          }
-        });
+              }
+            });
     endpointBuilder.setRootUrl(RealtimeModule.getEndpointRootUrl(serverAddress));
     return CloudEndpointUtils.updateBuilder(endpointBuilder).build();
   }
@@ -680,69 +684,68 @@ public class MainActivity extends RoboActivity {
 
   private boolean touchEvent(MotionEvent event) {
     switch (event.getAction()) {
-    case MotionEvent.ACTION_DOWN:
-      setLeftMenuLayoutX(0);
-      setLeftMenuLayoutX(-leftMenu.getWidth());
-
-      if (event.getX() < 40) {
-        showLeftMenuLayout();
-
-        startPoint = event.getX();
-        isShow = true;
-      }
-
-      break;
-    case MotionEvent.ACTION_UP:
-      if ((Math.abs(leftMenu.getLeft()) <= leftMenu.getWidth() / 3) && leftMenu.getVisibility() == View.VISIBLE) {
+      case MotionEvent.ACTION_DOWN:
         setLeftMenuLayoutX(0);
-        middleLayout.setVisibility(View.VISIBLE);
-      } else {
-        hideLeftMenuLayout();
-      }
+        setLeftMenuLayoutX(-leftMenu.getWidth());
 
-      startPoint = 0;
-      isShow = false;
+        if (event.getX() < 40) {
+          showLeftMenuLayout();
 
-      break;
-    case MotionEvent.ACTION_MOVE:
-      do {
-        if (!isShow) {
-
-          break;
+          startPoint = event.getX();
+          isShow = true;
         }
 
-        if (Math.abs(event.getX() - startPoint) < 3) {
-
-          break;
+        break;
+      case MotionEvent.ACTION_UP:
+        if ((Math.abs(leftMenu.getLeft()) <= leftMenu.getWidth() / 3)
+            && leftMenu.getVisibility() == View.VISIBLE) {
+          setLeftMenuLayoutX(0);
+          middleLayout.setVisibility(View.VISIBLE);
+        } else {
+          hideLeftMenuLayout();
         }
 
-        if (leftMenu.getLeft() >= 0) {
+        startPoint = 0;
+        isShow = false;
 
-          break;
-        }
-
-        if (startPoint < event.getX()) {
-          int add = leftMenu.getLeft() + (int) Tools.getRawSize(TypedValue.COMPLEX_UNIT_DIP, 6);
-          if (add < 0) {
-            setLeftMenuLayoutX(add);
-          } else {
-            setLeftMenuLayoutX(0);
-            middleLayout.setVisibility(View.VISIBLE);
+        break;
+      case MotionEvent.ACTION_MOVE:
+        do {
+          if (!isShow) {
+            break;
           }
-        } else if (startPoint > event.getX()) {
-          int reduce = leftMenu.getLeft() - (int) Tools.getRawSize(TypedValue.COMPLEX_UNIT_DIP, 6);
-          if (Math.abs(reduce) < leftMenu.getWidth()) {
-            setLeftMenuLayoutX(reduce);
+
+          if (Math.abs(event.getX() - startPoint) < 3) {
+            break;
           }
-        }
 
-        startPoint = event.getX();
-      } while (false);
+          if (leftMenu.getLeft() >= 0) {
+            break;
+          }
 
-      break;
-    default:
+          if (startPoint < event.getX()) {
+            int add = leftMenu.getLeft() + (int) Tools.getRawSize(TypedValue.COMPLEX_UNIT_DIP, 6);
+            if (add < 0) {
+              setLeftMenuLayoutX(add);
+            } else {
+              setLeftMenuLayoutX(0);
+              middleLayout.setVisibility(View.VISIBLE);
+            }
+          } else if (startPoint > event.getX()) {
+            int reduce =
+                leftMenu.getLeft() - (int) Tools.getRawSize(TypedValue.COMPLEX_UNIT_DIP, 6);
+            if (Math.abs(reduce) < leftMenu.getWidth()) {
+              setLeftMenuLayoutX(reduce);
+            }
+          }
 
-      break;
+          startPoint = event.getX();
+        } while (false);
+
+        break;
+      default:
+
+        break;
     }
 
     return true;
