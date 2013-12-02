2fb0cfbbd11b224863969145e7881c52afae01e8
larry <i@goodow.com>
2013-12-02 20:38:34 +0800

    Remove old dependencies
    Change-Id: Ie117ea7b03f6b888585c1cacc115adb4b1c815a9

--- src/main/java/com/goodow/drive/android/Interface/IOnItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.goodow.drive.android.Interface;
-
-import com.goodow.realtime.CollaborativeMap;
-
-public interface IOnItemClickListener {
-  public void onItemClick(CollaborativeMap file);
-}
