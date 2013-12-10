6be9271b731eee021c7980a2408a1199a6aa3b57
danhantao <danhantao@goodow.com>
2013-12-10 14:46:52 +0800

    调节音量
    Change-Id: I5d6c8881643272d735767ec9bd9dc4fba71fb7bb

--- src/main/java/com/goodow/drive/android/settings/SettingsRegistry.java
+++ src/main/java/com/goodow/drive/android/settings/SettingsRegistry.java
@@ -5,8 +5,11 @@ import com.goodow.realtime.channel.EventHandler;
 import com.goodow.realtime.json.JsonObject;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.util.Log;
 
 public class SettingsRegistry {
+  private final static String TAG = SettingsRegistry.class.getSimpleName();
   private EventBus mEventBus;
   private String mSid;
   private Context mContext;
@@ -22,7 +25,29 @@ public class SettingsRegistry {
     mEventBus.registerHandler(mSid + "drive.settings.audio", new EventHandler<JsonObject>() {
       @Override
       public void handler(JsonObject message, EventHandler<JsonObject> reply) {
-
+        AudioManager mAudioManager =
+            (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        if (message.has("mute")) {
+          mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, AudioManager.FLAG_SHOW_UI);
+        } else if (message.has("volume")) {
+          double volume = message.getNumber("volume");
+          mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (mAudioManager
+              .getStreamMaxVolume(AudioManager.STREAM_MUSIC) * volume), AudioManager.FLAG_SHOW_UI);
+        } else if (message.has("increase")) {
+          double inCrease = message.getNumber("increase");
+          mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (mAudioManager
+              .getStreamVolume(AudioManager.STREAM_MUSIC) + mAudioManager
+              .getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+              * inCrease), AudioManager.FLAG_SHOW_UI);
+          Log.i(TAG, mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) + "");
+        } else if (message.has("decrease")) {
+          double decrease = message.getNumber("decrease");
+          mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (mAudioManager
+              .getStreamVolume(AudioManager.STREAM_MUSIC) - mAudioManager
+              .getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+              * decrease), AudioManager.FLAG_SHOW_UI);
+          Log.i(TAG, mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) + "");
+        }
       }
     });
   }
