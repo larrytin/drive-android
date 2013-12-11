ab96bfbfad422a387ea2af593345bd98992b7a8d
danhantao <danhantao@goodow.com>
2013-12-11 16:34:02 +0800

    3G定位
    Change-Id: Ica05986dfaab10ed39e2e421bedb32a06b778af1

--- src/main/java/com/goodow/drive/android/settings/SettingsRegistry.java
+++ src/main/java/com/goodow/drive/android/settings/SettingsRegistry.java
@@ -3,10 +3,16 @@ package com.goodow.drive.android.settings;
 import com.goodow.drive.android.BusProvider;
 import com.goodow.realtime.channel.EventBus;
 import com.goodow.realtime.channel.EventHandler;
+import com.goodow.realtime.json.Json;
 import com.goodow.realtime.json.JsonObject;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+import android.telephony.cdma.CdmaCellLocation;
+import android.telephony.gsm.GsmCellLocation;
 import android.util.Log;
 
 public class SettingsRegistry {
@@ -46,6 +52,42 @@ public class SettingsRegistry {
               * decrease), AudioManager.FLAG_SHOW_UI);
           Log.i(TAG, mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC) + "");
         }
+      }
+    });
+    eb.registerHandler(PREFIX + "location", new EventHandler<JsonObject>() {
+      @Override
+      public void handler(JsonObject message, EventHandler<JsonObject> reply) {
+        JsonObject msg = Json.createObject();
+        ConnectivityManager mConnectivityManager =
+            (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = mConnectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+          msg.set("NetWorkType", networkInfo.getTypeName());
+        } else if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+          msg.set("NetWorkType", networkInfo.getTypeName());
+          TelephonyManager mTelephonyManager =
+              (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+          // 返回值MCC + MNC
+          String operator = mTelephonyManager.getNetworkOperator();
+          msg.set("MCC+MNC", operator);
+          int MCC = Integer.parseInt(operator.substring(0, 3));
+          int MNC = Integer.parseInt(operator.substring(3));
+          if (MNC == 0 || MCC == 1) {
+            // 中国移动和中国联通获取LAC、CID的方式
+            GsmCellLocation gsmLocation = (GsmCellLocation) mTelephonyManager.getCellLocation();
+            msg.set("CID", gsmLocation.getCid());
+            msg.set("LAC", gsmLocation.getLac());
+            msg.set("PSC", gsmLocation.getPsc());
+          } else if (MNC == 3) {
+            CdmaCellLocation cdmaLocation = (CdmaCellLocation) mTelephonyManager.getCellLocation();
+            msg.set("BID", cdmaLocation.getBaseStationId());
+            msg.set("NID", cdmaLocation.getNetworkId());
+            msg.set("SID", cdmaLocation.getSystemId());
+            msg.set("Latitude", cdmaLocation.getBaseStationLatitude());
+            msg.set("Longitude", cdmaLocation.getBaseStationLongitude());
+          }
+        }
+        reply.handler(msg, null);
       }
     });
   }
