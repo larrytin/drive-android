c918944375e0c8783b244eb4ef9e10efa739967a
danhantao <danhantao@goodow.com>
2013-12-23 09:30:48 +0800

    将首页写到HomeActivity中
    Change-Id: I6a2177cfc2f0ba2989fc7cee0eef7a23930f2c33

---
+++ src/main/java/com/goodow/drive/android/activity/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.goodow.drive.android.activity;
+
+import com.goodow.android.drive.R;
+
+import android.os.Bundle;
+
+public class HomeActivity extends BaseActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.setContentView(R.layout.activity_home);
+  }
+}
--- src/main/java/com/goodow/drive/android/activity/ViewRegistry.java
+++ src/main/java/com/goodow/drive/android/activity/ViewRegistry.java
@@ -2,8 +2,12 @@ package com.goodow.drive.android.activity;
 
 import com.goodow.drive.android.BusProvider;
 import com.goodow.realtime.channel.Bus;
+import com.goodow.realtime.channel.Message;
+import com.goodow.realtime.channel.MessageHandler;
+import com.goodow.realtime.json.JsonObject;
 
 import android.content.Context;
+import android.content.Intent;
 
 public class ViewRegistry {
   public static final String PREFIX = BusProvider.SID + "view.";
@@ -15,5 +19,14 @@ public class ViewRegistry {
   }
 
   public void subscribe() {
+    bus.registerHandler(PREFIX + "home", new MessageHandler<JsonObject>() {
+      @Override
+      public void handle(Message<JsonObject> message) {
+        Intent intent = new Intent(ctx, HomeActivity.class);
+        intent.putExtra("msg", message.body());
+        ctx.startActivity(intent);
+      }
+    });
+
   }
 }
