4a120d49a9d3346477079a3fe3c3385e84199082
liudenghui <liudenghui@goodow.com>
2013-12-26 16:06:10 +0800

    设置布局,横竖屏,多分辨率,按钮可滑动
    Change-Id: Iea9eb0fdfdd60ad7c1675f28076ff00e8dee2e8b

--- src/main/java/com/goodow/drive/android/activity/SettingActivity.java
+++ src/main/java/com/goodow/drive/android/activity/SettingActivity.java
@@ -1,14 +1,16 @@
 package com.goodow.drive.android.activity;
 
+import com.goodow.android.drive.R;
+import com.goodow.drive.android.settings.SettingsRegistry;
+import com.goodow.realtime.channel.Bus;
+import com.goodow.realtime.json.Json;
+import com.goodow.realtime.json.JsonObject;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
-import com.goodow.android.drive.R;
-import com.goodow.realtime.channel.Bus;
-import com.goodow.realtime.json.Json;
-import com.goodow.realtime.json.JsonObject;
+import android.widget.ImageView;
 
 public class SettingActivity extends BaseActivity implements OnClickListener {
   private Button bt_setting_about;
@@ -16,30 +18,7 @@ public class SettingActivity extends BaseActivity implements OnClickListener {
   private Button bt_setting_resolution;
   private Button bt_setting_screen_offset;
   private Button bt_setting_wifi;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    this.setContentView(R.layout.setting_activity);
-    initView();
-    setListener();
-  }
-
-  private void initView() {
-    bt_setting_about = (Button) findViewById(R.id.bt_setting_about);
-    bt_setting_reboot = (Button) findViewById(R.id.bt_setting_reboot);
-    bt_setting_resolution = (Button) findViewById(R.id.bt_setting_resolution);
-    bt_setting_screen_offset = (Button) findViewById(R.id.bt_setting_screen_offset);
-    bt_setting_wifi = (Button) findViewById(R.id.bt_setting_wifi);
-  }
-
-  private void setListener() {
-    bt_setting_about.setOnClickListener(this);
-    bt_setting_reboot.setOnClickListener(this);
-    bt_setting_resolution.setOnClickListener(this);
-    bt_setting_screen_offset.setOnClickListener(this);
-    bt_setting_wifi.setOnClickListener(this);
-  }
+  private ImageView iv_common_back;
 
   @Override
   public void onClick(View view) {
@@ -57,7 +36,7 @@ public class SettingActivity extends BaseActivity implements OnClickListener {
         bus.send(Bus.LOCAL + ViewRegistry.PREFIX + "aboutUs", null, null);
         break;
       case R.id.bt_setting_reboot:// 重启
-        bus.send(Bus.LOCAL + ViewRegistry.PREFIX + "reboot", null, null);
+        bus.send(Bus.LOCAL + SettingsRegistry.PREFIX + "reboot", null, null);
         break;
       case R.id.iv_common_back://
         JsonObject msg = Json.createObject();
@@ -66,5 +45,31 @@ public class SettingActivity extends BaseActivity implements OnClickListener {
         break;
 
     }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.setContentView(R.layout.setting_activity);
+    initView();
+    setListener();
+  }
+
+  private void initView() {
+    bt_setting_about = (Button) findViewById(R.id.bt_setting_about);
+    bt_setting_reboot = (Button) findViewById(R.id.bt_setting_reboot);
+    bt_setting_resolution = (Button) findViewById(R.id.bt_setting_resolution);
+    bt_setting_screen_offset = (Button) findViewById(R.id.bt_setting_screen_offset);
+    bt_setting_wifi = (Button) findViewById(R.id.bt_setting_wifi);
+    iv_common_back = (ImageView) findViewById(R.id.iv_common_back);
+  }
+
+  private void setListener() {
+    bt_setting_about.setOnClickListener(this);
+    bt_setting_reboot.setOnClickListener(this);
+    bt_setting_resolution.setOnClickListener(this);
+    bt_setting_screen_offset.setOnClickListener(this);
+    bt_setting_wifi.setOnClickListener(this);
+    iv_common_back.setOnClickListener(this);
   }
 }
