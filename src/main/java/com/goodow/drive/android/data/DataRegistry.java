ced3dae8315dae37a8c8cd7280e82b1a49a7ec27
larry <i@goodow.com>
2014-01-07 21:52:26 +0800

    Change-Id: I24a0d310e9f6f8ff7ddbe5f70d76e0aa3225c324

--- src/main/java/com/goodow/drive/android/data/DataRegistry.java
+++ src/main/java/com/goodow/drive/android/data/DataRegistry.java
@@ -40,7 +40,7 @@ public class DataRegistry {
           if (result) {
             body.set("status", "ok");
           }
-          bus.send(Bus.LOCAL + "" + Bus.LOCAL + Constant.ADDR_TOPIC, body, null);
+          bus.send(Bus.LOCAL + Bus.LOCAL + Constant.ADDR_TOPIC, body, null);
           return;
         }
 
